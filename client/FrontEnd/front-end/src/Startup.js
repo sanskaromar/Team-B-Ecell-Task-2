3233c8fafc0d97055022074962957d3c1f06f678
Sambhav-Sam <97016770+Sambhav-Sam@users.noreply.github.com>
2023-01-03 14:58:24 +0530

    Update Startup.js

--- client/FrontEnd/front-end/src/Startup.js
+++ client/FrontEnd/front-end/src/Startup.js
@@ -11,7 +11,7 @@ const Startup = () => {
         speed: 500,
         slidesToShow: 4,
         slidesToScroll: 1,
-        // autoplay: true,
+        autoplay: true,
         responsive:[
           {
             breakpoint: 730,
