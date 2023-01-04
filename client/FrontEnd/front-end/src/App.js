88c899e19940d7d9635e7228b3ecc6a777f6f748
Sanchali Sharma <sharmasanchali06@gmail.com>
2023-01-04 16:39:46 +0530

    imported info component

--- client/FrontEnd/front-end/src/App.js
+++ client/FrontEnd/front-end/src/App.js
@@ -5,6 +5,7 @@ import StartupToken from './components/independent-components/StartupToken';
 import Header from './components/dependent-components/Header';
 import logo from './assets/quiz.jpg';
 import bulbImage from './assets/bulbImage.jpg';
+import Info from './components/independent-components/Info'
 
 import EventsToken from './components/independent-components/EventsToken';
 import AboutUs from './components/dependent-components/AboutUs';
@@ -14,8 +15,9 @@ function App() {
 
   return (
     <div>
-      <Header background={bulbImage} />
-      <AboutUs />
+      {/* <Header background={bulbImage} />
+      <AboutUs /> */}
+      <Info/>
     </div>
   );
 }
